b1e9b8c45d38c9435eada1d03b29cf4281a76740
andreamarchetto04 <72484739+andreamarchetto04@users.noreply.github.com>
2023-03-08 09:19:55 +0100

    .
    .

--- backend/model/user.php
+++ backend/model/user.php
@@ -18,9 +18,10 @@ class User
         $sql = sprintf("SELECT nickname, mail, pw, id
         FROM `user`
         where 1=1");
+
         $result = $this->conn->query($sql);
         while ($row = $result->fetch_assoc()) {
-            if ($this->conn->real_escape_string($nickname) == $this->conn->real_escape_string($row["nickname"]) && $this->conn->real_escape_string($row["pw"]) == $this->conn->real_escape_string($pw)) {
+            if ($this->conn->real_escape_string($nickname) == $this->conn->real_escape_string($row["nickname"]) && $this->conn->real_escape_string($row["mail"]) == $this->conn->real_escape_string($mail) && $this->conn->real_escape_string($row["pw"]) == $this->conn->real_escape_string($pw)) {
                 return $this->conn->real_escape_string($row["id"]);
             }
         }
