d516e5f5ebe145554f5f68e8e3e09b610ea6f6e0
andreamarchetto04 <72484739+andreamarchetto04@users.noreply.github.com>
2023-03-17 15:44:03 +0100

    listone
    listone funzionante

--- frontend/pages/listone.php
+++ frontend/pages/listone.php
@@ -19,7 +19,7 @@
 
     <div class="container-fluid">
         <div class="row">
-            
+
             <div class="col-10">
                 <div class="row">
                     <h1 class="title text-center" id="title_table">
@@ -31,8 +31,8 @@
                             <thead>
                                 <tr>
                                     <th scope="col">quotation</th>
-                                    <th scope="col">name</th>                    
-                                    <th scope="col">role</th> 
+                                    <th scope="col">name</th>
+                                    <th scope="col">role</th>
                                     <th scope="col">team</th>
                                     <th></th>
                                 </tr>
@@ -45,11 +45,7 @@
         </div>
     </div>
 
-
-    <?php require_once(__DIR__ . '\footer.php'); ?>
     <?php
-
-
     include_once dirname(__FILE__) . '/../function/player.php';
 
     $player_arr = getArchivePlayer();
@@ -57,25 +53,15 @@
     if (!empty($player_arr) && $player_arr != -1) {
         foreach ($player_arr as $row) {
             echo ('<tr>');
-            //if( $i++ < 1){
-              //  continue; //skippo gli elementi che non servono
-            //}
-            //else{
             foreach ($row as $cell) {
-             //if($cell == $row['quotation']){
-                // continue; //skippo gli elementi che non servono
-            // }
-             //else{
-                  //ogni elemento della riga è finalmente una cella
                 echo ('<td>' . $cell . '</td>');
-             //}
-         //}
+            }
         }
-        }
-        echo('</tbody>'); 
+        echo ('</tbody>');
         echo ('</table>');
     }
     ?>
+    <?php require_once(__DIR__ . '\footer.php'); ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
